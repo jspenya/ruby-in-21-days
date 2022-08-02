6640e3b7fce73663d9d096fb9675161b3a2a8fac
Jose Stephen Peña <jose.pena@cybersoftbpo.com>
2022-08-02 13:59:19 +0800

    top-level/private methods

--- day_2_hello_objects/top_level_method_sample.rb
+++ day_2_hello_objects/top_level_method_sample.rb
@@ -8,9 +8,29 @@ end
 # and let that method call foo
 obiwan = Object.new
 def obiwan.call_foo
+	puts 'Foo from object: '
 	foo
 end
 
 obiwan.call_foo
 
 # Same thing on what we did above
+# but instead of doing a single object,
+# we define a class and make a couple of instances of it!
+
+class FooCaller
+	def call_foo
+		puts 'Foo from class: '
+		foo
+	end
+end
+
+foo_caller1 = FooCaller.new
+foo_caller1.call_foo
+
+# This generates an error
+# Private methods cannot have a receiver specified (in this case 'obiwan' is our receiver)
+# When no receiver is specified, whatever object made the call is considered the receiver
+# foo <-- the receiver here is self or 'main'
+# p self
+obiwan.foo
